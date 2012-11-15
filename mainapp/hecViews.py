e5776a8e91c4e88b8c038b362211ade6dcf2b3a5
gkrishna <krishna.gaurav43@gmail.com>
2012-11-15 19:08:55 +0530

    Added message not properly functioning right now some buggs

--- mainapp/hecViews.py
+++ mainapp/hecViews.py
@@ -8,28 +8,3 @@ from mainapp.forms import ComplaintForm
 import datetime
 
 
-def create_boudget(request) :
-
-
-def modify_boudget(request, id) :
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-def create_mess_menu(request) :
-
-
-def modify_mess_menu(request, id) :
-
-
-def create_mess_bill(request) :
-
-
-def modify_mess_bill(request,id) :
-
-
-def arrange_meeting(request) :
-
-
-def modify_meeting(request, id) :
-    
-    
-                     
